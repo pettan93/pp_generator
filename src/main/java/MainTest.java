990f06cc40029629dc0dfeb3ef4a2470ace23c37
Pettan93 <panhammer@seznam.cz>
2017-06-13 20:20:52 +0200

    Zipper

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,4 +1,10 @@
+import org.apache.commons.compress.archivers.zip.*;
+import org.apache.commons.compress.parallel.InputStreamSupplier;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -47,7 +53,7 @@ public class MainTest {
 
         System.out.println("juch");
         taskPicker.setTasks(new ArrayList<>(TaskResultWriter.generatedTasks));
-        TaskResultWriter.generatedTasks = new ArrayList<>();
+        TaskResultWriter.generatedTasks = new ArrayLists<>();
 
         ZipOutputStream zipOutputStream = Zipper.startZipping("test.zip");
         TaskExecuter.zip = zipOutputStream;
@@ -75,6 +81,34 @@ public class MainTest {
         Long end = System.currentTimeMillis();
         System.out.println("Hotovo za [" + (end - start) / 1000 + " sekund]");
 
+
+
+
+
+    }
+
+
+    public class ScatterSample {
+
+        ParallelScatterZipCreator scatterZipCreator = new ParallelScatterZipCreator();
+        ScatterZipOutputStream dirs = ScatterZipOutputStream.fileBased(File.createTempFile("scatter-dirs", "tmp"));
+
+        public ScatterSample() throws IOException {
+        }
+
+        public void addEntry(ZipArchiveEntry zipArchiveEntry, InputStreamSupplier streamSupplier) throws IOException {
+            if (zipArchiveEntry.isDirectory() && !zipArchiveEntry.isUnixSymlink())
+                dirs.addArchiveEntry(ZipArchiveEntryRequest.createZipArchiveEntryRequest(zipArchiveEntry, streamSupplier));
+            else
+                scatterZipCreator.addArchiveEntry( zipArchiveEntry, streamSupplier);
+        }
+
+        public void writeTo(ZipArchiveOutputStream zipArchiveOutputStream)
+                throws IOException, ExecutionException, InterruptedException {
+            dirs.writeTo(zipArchiveOutputStream);
+            dirs.close();
+            scatterZipCreator.writeTo(zipArchiveOutputStream);
+        }
     }
 
 
